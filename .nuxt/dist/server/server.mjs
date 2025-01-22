db1310977aedabc1de8846d4d95558b94b35ac09
Haroldd-joness <jonesharold421@gmail.com>
2025-01-22 17:54:22 +0000

    Updates

--- .nuxt/dist/server/server.mjs
+++
@@ -1,1 +0,0 @@
-export { default } from "file:///C:/Users/jones/OneDrive/Desktop/Spacee/node_modules/@nuxt/vite-builder/dist/runtime/vite-node.mjs"
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,10 @@
+import { SpeedInsights } from "@vercel/speed-insights/nuxt"
 <template>
   <CommonCursor />
   <CommonProgressScroll />
+
+  <SpeedInsights />
+  
   <slot />
 </template>
+
